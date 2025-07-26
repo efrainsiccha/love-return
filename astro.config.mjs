59a2d87d9de7cca6613a7f340d639c0bbff465a0
Efrain Siccha <163354946+efrainsiccha@users.noreply.github.com>
2025-07-26 15:14:39 -0500

    Switch to gh-pages deployment method

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'static',
   site: 'https://efrainsiccha.github.io',
   base: '/love-return',
   vite: {
